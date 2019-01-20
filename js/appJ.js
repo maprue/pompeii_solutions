3ddf27e965288df1733868a605599e885854a271
maprue <prudhomme.marie.anne@gmail.com>
2019-01-20 02:25:33 -0500

    Tweeks to the UI

--- js/appJ.js
+++ js/appJ.js
@@ -33,7 +33,8 @@ function autocomplete(inp, arr) {
                 /*close the list of autocompleted values,
                 (or any other open lists of autocompleted values:*/
                 closeAllLists();
-            });
+                displayCarbonFootprint();
+                 });
             a.appendChild(b);
           }
         }
@@ -94,3 +95,57 @@ function autocomplete(inp, arr) {
         closeAllLists(e.target);
     });
   }
+
+function displayCarbonFootprint() {
+  // document.body.innerHTML = document.body.innerHTML + "MY ANACONDA DONT";
+
+        var ctx = document.getElementById("myChart");
+        ctx.height = 5;
+        
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+         
+         ctx.style.backgroundColor = 'rgba(255,255,255, 0.9)';
+         
+       
+// var div = document.createElement("myChart");
+
+// document.getElementById("container").appendChild(myChart);
+
+
+}
